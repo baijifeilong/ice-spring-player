494a9c03800380d8e7a79d789205b682bf2f96a9
BaiJiFeiLong <baijifeilong@gmail.com>
2019-01-22 23:46:08 +0800

    Remove some unused code

--- renderer.js
+++ renderer.js
@@ -1,8 +1,4 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-const {remote, ipcRenderer} = require("electron");
+const {remote} = require("electron");
 const app = remote.app;
 const fs = require("fs");
 const path = require("path");
